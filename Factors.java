bab60583a120a428a5d17ab5b6d818b208281eba
SanjanaBhandure <sweetybhandure@gmail.com>
2022-04-12 21:41:57 +0530

    Added: Prints the prime factors of number N

---
+++ Factors.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz.Day05PracticeProblems;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args){
+            System.out.println("Taking number from user to find out their factor"); // 12 = 2, 3, 4, 6, 12 /2
+            Scanner sc = new Scanner(System.in);
+            int factorNumber = sc.nextInt();
+            for (int i = 2; i <= factorNumber; i++) {
+            if ( factorNumber % i == 0)
+                System.out.print(i + "\t");
+        }
+    }
+}
